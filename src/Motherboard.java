b69b0438fb22a435a7d10e1fd9be0885a01fdcaf
HasiTheCoder <105689152+HasiTheCoder@users.noreply.github.com>
2023-05-20 18:49:18 -0400

    Commit
    -Added Messages class to contain all the error and warning messages for all different cases.
    -Edited Motherboard class. Most of the attributes should be completed and just look over and check what is remaining.

--- src/Motherboard.java
+++ src/Motherboard.java
@@ -1,14 +1,28 @@
 public class Motherboard extends PCComponent{
+    //All motherboards have power switch, power led, and drive led headers for the front panel
+    //AMD vs. Intel Socket and which gen socket
     String socketType;
+    //AMD vs. Intel chipset and which gen chipset
     String chipSet;
+    //Number of memory slots
     int memorySlots;
-    int PCIe16xSlots;
-    int PCIe8xSlots;
-    int PCIe4xSlots;
-    int PCIe1xSlots;
+    int PCIeSlots;
     int PCISlots;
     int SATAConnectors;
     int M_2Slots;
     int _24PinPowerConnectors;
-    int _
+    boolean is4Pin;
+    int fanHeaders;
+    //USB 3.1 Gen 1 Headers
+    int USB3Headers;
+    //USB 2.0 Headers
+    int USB2Headers;
+    //USB 3.1 Gen 2 Headers
+    int USB3G2Headers;
+    //USB-C Headers
+    int USB_CHeaders;
+    //Audio Headers for Mic and Headphones
+    int frontAudioHeaders;
+    int RGBHeaders;
+
 }
