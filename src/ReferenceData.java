60b62caa030580db6b6ff8612adc4eed1024ec3d
HasiTheCoder <105689152+HasiTheCoder@users.noreply.github.com>
2023-06-04 00:21:34 -0400

    Reference data class made. Need to implement reading JSON

---
+++ src/ReferenceData.java
@@ -0,0 +1,16 @@
+public class ReferenceData {
+    public Case[] Cases = new Case[0];
+    public CaseFans[] CaseFans = new CaseFans[0];
+    public CPU[] CPUs = new CPU[0];
+    public CPUCooler[] CPUCoolers = new CPUCooler[0];
+    public GPU[] GPUs = new GPU[0];
+    public Motherboard[] Motherboards = new Motherboard[0];
+    public PowerSupply[] PowerSupplies = new PowerSupply[0];
+    public MemoryKits[] MemoryKits = new MemoryKits[0];
+    public Storage[] Storages = new Storage[0];
+    public ReferenceData() {
+        
+    }
+    //TODO:
+    //Create one method to read one json file with all the reference data split into sections by type
+}
